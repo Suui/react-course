1e3648fc86da34d2c937b98e536478c8f7efa540
Suui <suui.gd@gmail.com>
2016-12-16 00:33:05 +0100

    Error on createAuthorRow

---
+++ react-flux/src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'core-house',
+            firstName: 'Cory',
+            lastName: 'House'
+        },
+        {
+            id: 'scott-allen',
+            firstName: 'Scott',
+            lastName: 'Allen'
+        },
+        {
+            id: 'dan-wahlin',
+            firstName: 'Dan',
+            lastName: 'Wahlin'
+        }
+    ]
+};
